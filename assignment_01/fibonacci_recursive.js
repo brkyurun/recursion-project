694feed9be84f26e3149d3f75e8f14be414b7d11
berkay <berkayurun@outlook.com>
2022-08-09 11:28:07 +0300

    return an array instead of a single number

--- assignment_01/fibonacci_recursive.js
+++ assignment_01/fibonacci_recursive.js
@@ -5,15 +5,3 @@ function fibonacci(n, fibonacciArray = [0, 1]) {
   fibonacciArray.push(nextFibonacciNumber);
   return fibonacci(n, fibonacciArray);
 }
-
-console.log(fibonacci(7));
-
-// const fibsRec = (num, sequence = [0, 1]) => {
-//   if (sequence.length >= num) {
-//     return sequence.slice(0, num + 1);
-//   }
-
-//   const newNumber = sequence.at(-1) + sequence.at(-2);
-//   sequence.push(newNumber);
-//   return fibsRec(num, sequence);
-// }
